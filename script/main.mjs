db72c1e52d4aed4851b80c3763d0fe198509fdc0
CasHutten <cashutten@hotmail.nl>
2019-04-03 00:58:39 +0200

    fixed bullet rendering

--- script/main.mjs
+++ script/main.mjs
@@ -2,9 +2,9 @@
 
 import {Bullet} from "./objects/bullet.mjs";
 
-const   bullet1 = new Bullet(1, 'media/combat_test/seed.png'),
-        bullet2 = new Bullet(2, 'media/combat_test/seed.png'),
-        bullet3 = new Bullet(3, 'media/combat_test/seed.png');
+const   bullet1 = new Bullet(1, 5, 'media/combat_test/seed.png'),
+        bullet2 = new Bullet(2, 5, 'media/combat_test/seed.png'),
+        bullet3 = new Bullet(3, 5, 'media/combat_test/seed.png');
 
 const gameFrame = document.getElementById('gameFrame');
 const ctx = gameFrame.getContext('2d');
@@ -13,23 +13,23 @@ const ctx = gameFrame.getContext('2d');
 // Game loop
 // ----------
 function update(elapsed) {
+
   bullet1.shoot('diagonal');
   bullet2.shoot('horizontal');
   bullet3.shoot('vertical');
 }
 
 function render() {
-  ctx.clearRect(0, 0, gameFrame.clientWidth, gameFrame.height);
+  ctx.clearRect(0, 0, 600, 800);
 
-  bullet1.draw();
-  bullet2.draw();
-  bullet3.draw();
+  bullet1.draw(ctx);
+  bullet2.draw(ctx);
+  bullet3.draw(ctx);
 }
 
 let lastUpdate;
 
 function tick() {
-  window.requestAnimationFrame(tick);
   let now = window.Date.now();
 
   if (lastUpdate) {
@@ -46,6 +46,7 @@ function tick() {
   }
 
   // This makes the `tick` function run 60 frames per second (or slower, depends on monitor's refresh rate).
+  window.requestAnimationFrame(tick);
 };
 
 window.requestAnimationFrame(tick);
--- script/objects/bullet.mjs
+++ script/objects/bullet.mjs
@@ -1,34 +1,31 @@
 export class Bullet {
-    constructor(id, looks) {
+    constructor(id, speed, looks) {
         this.id = id;
         this.src = looks;
         this.x = 0;
         this.y = 0;
+        this.speed = speed;
     }
 
-    draw() {
-        const ctx = gameFrame.getContext('2d');
+    draw(ctx) {
         let bulletRender = new Image();
         let x = this.x, y = this.y;
 
-        bulletRender.onload = function() {
-            ctx.drawImage(bulletRender, x, y);
-        };
         bulletRender.src = this.src;
-
+        ctx.drawImage(bulletRender, x, y);
     }
 
     shoot(direction) {
         switch (direction) {
             case 'horizontal':
-                this.x += 1;
+                this.x += this.speed;
                 break;
             case 'vertical':
-                this.y += 1;
+                this.y += this.speed;
                 break;
             case 'diagonal':
-                this.x += 1;
-                this.y += 1;
+                this.x += this.speed;
+                this.y += this.speed;
                 break;
         }
     }
